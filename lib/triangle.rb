24e831098f2370980dab9695cfe65402be969277
Alexander Schelchere <ascellie@gmail.com>
2019-06-12 17:37:02 -0400

    A triangle knows what size it is

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,14 @@
 class Triangle
-  # write code here
+    attr_reader :side1, :side2, :side3
+
+    def initialize(side1, side2, side3)
+      @side1, @side2, @side3 = side1, side2, side3
+    end
+
+    def kind
+      return :scalene if side1 != side2 and side1 != side3 and side2 != side3
+      return :equilateral if side1 == side2 and side1 == side3
+      :isosceles 
+    end
+
 end
